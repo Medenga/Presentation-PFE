de4957c936721e673932659b836d218353a13bd5
nicop140 <nicolaspereira@dartybox.com>
2026-02-03 12:42:20 +0100

    majslide3

--- client/src/slides/Slide3.jsx
+++ client/src/slides/Slide3.jsx
@@ -51,7 +51,7 @@ const Slide3 = () => {
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             transition={{ delay: 0.4 }}
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+            className="absolute top-[58%] left-1/2 -translate-x-1/2 -translate-y-1/2 
                        w-[90px] h-[90px] bg-[#FFE600] rounded-full 
                        shadow-[0_0_40px_rgba(255,230,0,0.6)] 
                        flex items-center justify-center z-10"
